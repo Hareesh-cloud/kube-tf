1e1bf5aa91d8da8b27cd8eabbf5599508fca5e64
Hareesh5041 <82581268+Hareesh5041@users.noreply.github.com>
2021-06-09 15:58:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,41 +12,16 @@ resource "null_resource" "gcloud" {
   }
 }
 
-# Deploy image to Cloud Run
-resource "google_cloud_run_service" "mywebapp" {
-  name     = "mywebapp"
-  location = "us-central1"
-  template {
-    spec {
-      containers {
-        image = "gcr.io/terraform-9999/tomcat"
-      }
-    }
-  }
-  traffic {
-    percent         = 100
-    latest_revision = true
-  }
+resource "google_container_cluster" "gke-cluster" {
+  name                = "my-first-gke-cluster"
+  network             = "default"
+  zone                = "us-central1-a"
+  initial_node_count  = 2
 }
 
-# Create public access
-data "google_iam_policy" "noauth" {
-  binding {
-    role = "roles/run.invoker"
-    members = [
-      "allUsers",
-    ]
+# Create build and run
+resource "null_resource" "kubectl" {
+  provisioner "local-exec" {
+    command = "kubectl apply -f deploy.yaml"
   }
-}
-# Enable public access on Cloud Run service
-resource "google_cloud_run_service_iam_policy" "noauth" {
-  location    = google_cloud_run_service.mywebapp.location
-  project     = google_cloud_run_service.mywebapp.project
-  service     = google_cloud_run_service.mywebapp.name
-  policy_data = data.google_iam_policy.noauth.policy_data
-}
-
-# Return service URL
-output "url" {
-  value = "${google_cloud_run_service.mywebapp.status[0].url}"
 }
